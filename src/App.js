e4793823f957d24885adc0780e36671460b9b443
sarahkittyy <chrisohlin1@gmail.com>
2019-04-05 09:29:21 -0400

    added twitter handle

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends Component
 	{
 		return (
 			<div id="app">
-				<h1>Heather is really cute and I love her {"<3"}</h1>
+				<h1>Heather <a href="https://twitter.com/mdrnpxls">(@mdrnpxls)</a> is really cute and I love her~{"<3"}</h1>
 			</div>	
 		);
 	}
